835e17af8ab18afd0a2dbb804cdf579cf44f8589
sukantahui <huisukanta77@gmail.com>
2021-04-28 13:57:24 +0530

    Student registration course model modified

--- app/Models/StudentCourseRegistration.php
+++ app/Models/StudentCourseRegistration.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class StudentCourseRegistration extends Model
 {
     use HasFactory;
+
+    public function student()
+    {
+        return $this->belongsTo(Ledger::class,'ledger_id');
+    }
+    public function course()
+    {
+        return $this->belongsTo(Course::class,'course_id');
+    }
 }
--- database/seeders/StudentCourseRegistrationSeeder.php
+++ database/seeders/StudentCourseRegistrationSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\StudentCourseRegistration;
 use Illuminate\Database\Seeder;
 
 class StudentCourseRegistrationSeeder extends Seeder
@@ -13,6 +14,6 @@ class StudentCourseRegistrationSeeder extends Seeder
      */
     public function run()
     {
-        //
+        StudentCourseRegistration::create(['ledger_id'=>1,'course_id'=>1,'base_fee'=>3000,'discount_allowed'=>1200,'joining_date'=>'2021-04-28']);
     }
 }
